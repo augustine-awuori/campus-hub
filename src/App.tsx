822dedb2dca186eb53706404d5caec261a5f217e
Augustine Awuori <codewithaugustine@gmail.com>
2024-05-09 11:46:16 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Text fontSize={50} fontFamily="quicksand" textAlign="center">
+      Campus Hub
+    </Text>
+  );
+}
+
+export default App;
